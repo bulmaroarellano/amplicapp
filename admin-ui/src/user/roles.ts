cae280cd1177bd7b64a7411242d9492ca7847011
Bulmaro Arellano <67934295+bulmaroarellano@users.noreply.github.com>
2022-02-10 10:41:09 -0600

    amplication-build-ckzh7jitt42025501s6bv6qknje

---
+++ admin-ui/src/user/roles.ts
@@ -0,0 +1,14 @@
+export const ROLES = [
+  {
+    name: "superadmin",
+    displayName: "Super Admin",
+  },
+  {
+    name: "admin",
+    displayName: "Admin",
+  },
+  {
+    name: "employee",
+    displayName: "Employee",
+  },
+];
